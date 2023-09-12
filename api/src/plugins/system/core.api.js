422f84d82426a06c4b162255d53038e165661591
andre.matos <andre.matos@sisloc.com.br>
2023-09-12 13:30:34 -0300

    Projeto confeitaria

---
+++ api/src/plugins/system/core.api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+function Rest (config) {}
+
+module.exports = {
+    Rest,
+    async setup () {
+        console.log('Initializing API');
+    }
+}
