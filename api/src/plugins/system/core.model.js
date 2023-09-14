879f65350b2e91a8883f4c9934f63ec0479f74d8
andre.matos <andre.matos@sisloc.com.br>
2023-09-14 15:34:47 -0300

    Metodo de autenticação JWT

--- api/src/plugins/system/core.model.js
+++ api/src/plugins/system/core.model.js
@@ -1,9 +1,35 @@
-function Schema (config) {
-    return this
+const db = require('./core.db')
+
+const modelSchema = {}
+
+function Schema (schema) {
+  this.schema = schema
+  modelSchema[schema.name] = schema
+
+  this.get = async (id, ctx) => {
+    return db.document.get(id, ctx)
+  }
+
+  this.findOne = async (options = {}, ctx = {}) => {
+    options.where = options.where || {}
+    options.where.type = schema.name
+    return db.document.findOne(options, ctx)
+  }
+
+  this.findAll = async (options = {}, ctx = {}) => {
+    options.where = options.where || {}
+    options.where.type = schema.name
+    return db.document.findAll(options, ctx)
+  }
+
+  return this
 }
 
+
+
 module.exports = {
-    Schema,
-    String,
-    Date
+  schema: modelSchema,
+  Schema,
+  String,
+  Date
 }
--- api/src/plugins/system/models/user.model.js
+++ api/src/plugins/system/models/user.model.js
@@ -1,17 +1,18 @@
-const Model = require ('../core.model')
+const Model = require('../core.model')
 
 module.exports = new Model.Schema({
-    name: 'system/user',
-    model: {
-        email: {
-            type: Model.String,
-            label: "E-mail",
-            require: true
-        },
-        password:{
-            type: Model.String,
-            label: "Senha",
-            require: true
-        }
+  name: 'system/user',
+  label: 'Usuário',
+  model: {
+    email: {
+      type: Model.String,
+      label: 'E-mail',
+      required: true
+    },
+    password: {
+      type: Model.String,
+      label: 'Senha',
+      required: true
     }
+  }
 })
